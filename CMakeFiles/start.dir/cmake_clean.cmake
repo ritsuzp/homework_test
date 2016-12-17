0e4fd8a2107cc7d1b00bc35766aa2d94cd4c93a2
KNCheung <u201310616@hust.edu.cn>
2016-12-17 20:10:03 +0800

    乱七八糟的文件上传了没呢？

---
+++ CMakeFiles/start.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/start.dir/main.cpp.o"
+  "start.pdb"
+  "start"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
